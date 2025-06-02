3a5ad2f395170fc2e09ead60243c6ccb5a8419e9
micaelasu <msuarez@ans.edu.ni>
2025-06-01 23:28:36 -0600

    Update run-test.py

--- run-test.py
+++ run-test.py
@@ -1,7 +1,13 @@
-from checks import checkAddressType, checkUrlLength, checkShortening, checkAtSign, checkPortNumber, checkDomainAge, checkRedirects, checkDomainRegistration
-from mlforkidsnumbers import MLforKidsNumbers
+from checks import (
+    checkAddressType, checkUrlLength, checkShortening, 
+    checkAtSign, checkPortNumber, checkDomainAge, 
+    checkRedirects, checkDomainRegistration
+)
+from miforkidsnumbers import MLforkidsNumbers
 
-project = None
+# PASTE YOUR PROJECT DETAILS HERE (from ML for Kids website)
+project = "YOUR_PROJECT_ID"  # ← Replace with your project ID
+model = "YOUR_MODEL_NAME"    # ← Replace with your model name
 
 def checkUrl(addr):
     address_type = checkAddressType(addr)
@@ -13,42 +19,32 @@ def checkUrl(addr):
     redirects = checkRedirects(addr)
     domain_reg = checkDomainRegistration(addr)
 
-    print (addr)
-    print ("   address type :", address_type)
-    print ("   url length   :", url_length)
-    print ("   shortening   :", shortening)
-    print ("   includes @   :", includes_at)
-    print ("   port number  :", port_number)
-    print ("   domain age   :", domain_age)
-    print ("   redirects    :", redirects)
-    print ("   domain reg   :", domain_reg)
+    print("URL:", addr)
+    print("Address type:", address_type)
+    print("URL length:", url_length)
+    print("Shortening service:", shortening)
+    print("Includes @:", includes_at)
+    print("Port number:", port_number)
+    print("Domain age:", domain_age)
+    print("Redirects:", redirects)
+    print("Domain registration:", domain_reg)
 
-    addr_data = {
-        "address type" : address_type,
-        "url length" : url_length,
-        "shortening" : shortening,
-        "includes @" : includes_at,
-        "port number" : port_number,
-        "domain age" : domain_age,
-        "redirects" : redirects,
-        "domain reg" : domain_age,
-    }
+    # Classify using ML for Kids
+    ml = MLforkidsNumbers(project, model)
+    prediction = ml.classify({
+        "address_type": address_type,
+        "url_length": url_length,
+        "shortening": shortening,
+        "includes_at": includes_at,
+        "port_number": port_number,
+        "domain_age": domain_age,
+        "redirects": redirects,
+        "domain_reg": domain_reg
+    })
 
-    if project is not None:
-        predictions = project.classify(addr_data)
-        prediction = predictions[0]
-        print ("Prediction: %s is a %s link (%d%% confidence)" % (addr, prediction["class_name"], prediction["confidence"]))
+    print("Prediction:", prediction["class_name"])
+    print("Confidence:", prediction["confidence"], "%\n")
 
-    print ("")
-
-
-checkUrl("https://machinelearningforkids.co.uk/help")
-checkUrl("https://www.bbc.co.uk")
-
-checkUrl("https://en.wikipedia.org/wiki/Machine_learning")
-checkUrl("https://91.198.174.192/wiki/Machine_learning")
-
-checkUrl("https://mickey@bit.ly/35Jnlf8")
-
-checkUrl("https://login.bankofamerica.com@www.josueizagirre.com/wp-content/cache/login.bankofamerica.com.uplogad/update.html")
-checkUrl("http://flinxdeicdccc.com/2612aa892d962d6f8056b195ca6e550d/tnFBsV27sc3kIMJg0Z8snqenramMHIXz.php?country=US-United%20States&lang=en")
+# Test URLs
+checkUrl("https://www.bbc.com")  # Safe example
+checkUrl("http://paypal@phishing.com/login")  # Phishing example
